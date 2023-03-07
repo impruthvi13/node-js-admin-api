7c244573c4da3c59f80dbd57d32959e559907f43
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2023-03-07 13:35:42 +0530

    change in routes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./middlewares/errorMiddleware');
 const userRouter = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
@@ -70,7 +71,8 @@ app.use(xss());
 // );
 
 // 3) ROUTES
-app.use('/api/v1/users', userRouter);
+app.use('/api/admin/users', userRouter);
+app.use('/api', authRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,6 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
-
 // Protect all the routes after this middleware
 router.use(authController.protect);
 
